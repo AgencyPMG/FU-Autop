4521ff9b74f5ed20dd31d93155eaf1aeec34630b
Christopher Davis <cdavis9999@gmail.com>
2015-05-28 11:52:07 -0400

    Make the Plugin Composer Friendly

--- fu-autop.php
+++ fu-autop.php
@@ -1,29 +1,39 @@
 <?php
-/*
-Plugin Name: FU Autop
-Plugin URI: http://pmg.co
-Description: Because sometimes wpautop is a pain in the ass.
-Version: 1.0
-Text Domain: fuap
-Author: Christopher Davis
-Author URI: http://pmg.co/people/chris
-License: GPL2
-
-    Copyright 2012 Performance Media Group <seo@pmg.co>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Plugin Name: FU Autop
+ * Plugin URI: https://github.com/AgencyPMG/FU-Autop
+ * Description: Sometimes wpautop is a pain in the ass.
+ * Version: 1.0
+ * Text Domain: fuap
+ * Author: Christopher Davis
+ * Author URI: https://www.pmg.com
+ * License: MIT
+ *
+ * Copyright (c) 2015 PMG <https://www.pmg.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * @category    WordPress
+ * @author      Christopher Davis <http://christopherdavis.me>
+ * @copyright   2014 Christopher Davis
+ * @license     http://opensource.org/licenses/MIT MIT
+ */
 
 FU_Autop::init();
 
